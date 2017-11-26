8c4adf7a37b92bc6ad687962490ae44aeafd49f8
MUmer04 <umer_1309e@aptechgdn.com>
2017-11-26 10:10:03 -0800

    react events

--- react-initial/src/App.js
+++ react-initial/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import dd from './dd.JPG';
+// import dd from './dd.JPG';
 import './App.css';
 import Components from './Component.js';
 
-const Img = <img src = "dd.JPG" alt = "" />
+// const Img = <img src = "dd.JPG" alt = "" />
 class App extends Component {
  
   render() {
@@ -19,10 +19,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      
-        <Components text = {"Hello World"} />
-        <Components text = {"This is a component"} />
-        <Components text = {"This component has been reused"}/>
+{/*       
+        <Components text = {"Hello World"} >
+        This is msg1
+        </Components>
+        <Components text = {"This is a component"} >
+        This is mg 2
+        </Components>
+        <Components text = {"This component has been reused"}>
+        This is msg 3
+        </Components> */}
+        <Components />
         {/* <Components img = {Img}/> */}
     
       </div>
--- react-initial/src/Component.js
+++ react-initial/src/Component.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import dd from './dd.JPG';
+import './App.css'
+// import dd from './dd.JPG';
 
 class Components extends Component{
+    edit(){
+        alert("Editing Note");
+    }
+    remove(){
+        alert("Removing Note");
+    }
     render(){
         return(
-            <div>
-                <h1> {this.props.text} </h1>
-                {/* <p>{this.props.img}</p> */}
+            <div className="Button">
+                <span className="Padding"><button onClick = {this.edit}>Edit</button>
+                <button onClick = {this.remove}>X</button>
+                </span>
+                
+                {/* <h1> {this.props.text} </h1>
+                <p>{this.props.children}</p>
+                <p>{this.props.img}</p> */}
+
             </div>
         )
     }
